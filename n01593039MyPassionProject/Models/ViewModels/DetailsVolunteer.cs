04bd497639e9e337ff601e02010196c9aa414cc2
hatha <n01593039@humber.ca>
2024-02-24 04:22:53 -0500

    Add project files.

---
+++ n01593039MyPassionProject/Models/ViewModels/DetailsVolunteer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace n01593039MyPassionProject.Models.ViewModels
+{
+    public class DetailsVolunteer
+    {
+        public VolunteerDto SelectedVolunteer { get; set; }
+
+    }
+}
